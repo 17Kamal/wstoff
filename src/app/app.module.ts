f0e72b76d76e5f4e2ff3580ca5c4aa926f1384fe
Kamal <44814685+kamal847409@users.noreply.github.com>
2020-12-10 14:20:26 +0530

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { IconsProviderModule } from './icons-provider.module';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { hi_IN } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import {MatGridListModule} from '@angular/material/grid-list';
+import hi from '@angular/common/locales/hi';
+
+registerLocaleData(hi);
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    IconsProviderModule,
+    NzLayoutModule,
+    NzMenuModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatGridListModule
+  ],
+  providers: [{ provide: NZ_I18N, useValue: hi_IN }],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
